875fd00e815c4898a579208c5657f0b5735ae594
Koen Gilissen <gilissenkoen@gmail.com>
2017-08-20 23:39:35 +0200

    initial serial LCD interface
    Proof of concept
    Needs further LCD feature implementation

---
+++ lcdConstants.py
@@ -0,0 +1,16 @@
+#lcdConstants
+
+# controlCharacter 'FE'
+clearDisplay = bytearray.fromhex(u'FE 01') #  Clear Display
+moveCursorRight = bytearray.fromhex(u'FE 14')	 #  Move Cursor Right 1 Space
+moveCursorLeft = bytearray.fromhex(u'FE 10')	 #  Move Cursor Left 1 Space
+scrollRight = bytearray.fromhex(u'FE 1C')	 #  Scroll Right 1 Space
+scrollLeft = bytearray.fromhex(u'FE 18')	 #  Scroll Left 1 Space
+turnOffDisplay = bytearray.fromhex(u'FE 08')	 # Turn Display Off
+turnOnDisplay = bytearray.fromhex(u'FE 0C')	 #  Turn Display On / Cursor Off / Blinking Off
+turnOnDisplayCursor = bytearray.fromhex(u'FE 0E')	 #  Turn Display On / Cursor on / Blinking Off
+turnOnDisplayCursorBlinking = bytearray.fromhex(u'FE 0F')	 #  Turn Display On / Cursor on / Blinking On
+underlineCursor = bytearray.fromhex(u'FE 0E')	 #  Underline Cursor On
+blinkCursor = bytearray.fromhex(u'FE 0D')	 #  Blinking Cursor On
+cursorHomeLine0 = bytearray.fromhex(u'FE 80')	 # DD ram address 0x00
+cursorHomeLine1 = bytearray.fromhex(u'FE C0')	 # DD ran address 0x40
